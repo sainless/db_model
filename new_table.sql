a1057e45b3bf90bbea6082266360d00d5837e2f5
sainless <sainlesscode@gmail.com>
2025-05-15 23:38:58 +0300

    Create new_table.sql

---
+++ new_table.sql
@@ -0,0 +1,58 @@
+CREATE TABLE users (
+    id_user SERIAL PRIMARY KEY,
+    login_user VARCHAR(50) NOT NULL UNIQUE,
+    password_user VARCHAR(100) NOT NULL,
+    full_name VARCHAR(100),
+    data_registration TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    profile_photo VARCHAR(111)
+);
+
+
+CREATE TABLE base_information (
+    id_base SERIAL PRIMARY KEY,
+    email VARCHAR(50) NOT NULL UNIQUE,
+    city_living VARCHAR(100) NOT NULL,
+    number_phone VARCHAR(20),
+);
+
+
+CREATE TABLE personal_information (
+    id_personal SERIAL PRIMARY KEY,
+    gender VARCHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    age integer,
+    nationality VARCHAR(16),
+    marital_status VARCHAR(16),
+    position_s VARCHAR(16)
+
+);
+
+
+
+CREATE TABLE education (
+    id_education SERIAL PRIMARY KEY,
+    year text,
+    short_text varchar(100)
+);
+
+
+CREATE TABLE notes (
+    id_user SERIAL PRIMARY KEY,
+    employment varchar(10),
+    salary float
+);
+
+
+CREATE TABLE additional_information (
+    id_additional SERIAL PRIMARY KEY,
+    personal_qualities VARCHAR(50) NOT NULL ,
+    description text NOT NULL,
+    hobby VARCHAR(120),
+);
+
+
+CREATE TABLE programmer_skills (
+    id_skills SERIAL PRIMARY KEY,
+    languages text,
+    tech_skills text
+);
